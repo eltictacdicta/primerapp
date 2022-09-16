86d073be51c9eaa25eaf72b9e17bd2a859e91d1d
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-09-16 15:27:00 +0200

    Creamos el primer template

--- primerapp/polls/views.py
+++ primerapp/polls/views.py
@@ -1,6 +1,9 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 # Create your views here.
 
@@ -21,5 +24,10 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'questions' : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
