ac87533ce7b2f68a25a6b5370a69b73727a1aa96
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-09-21 15:30:47 +0200

    Empezamos a trabajar con las listas genericas

--- primerapp/polls/views.py
+++ primerapp/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 #from django.template import loader
 
 # Create your views here.
@@ -11,20 +12,20 @@ from django.urls import reverse
 """def index(request):
     return HttpResponse("Hola mundo")"""
 
+class IndexView(generic.ListView):
 
-def detail(request, question_id):
-    '''try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Esta página de detalle no existe")'''
-    question = get_object_or_404(Question, pk=question_id)
-    #choices = question.choice_set.all()
-    return render(request, 'polls/detail.html', {'question':question} )
-    #return HttpResponse("Detail %s" % question_id)
-    
-def results(request, question_id):
-    response = "Results %s" 
-    return HttpResponse(response % question_id)
+    template_name = 'polls/index.html'
+    context_object_name = 'questions'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DeatilView(generic.DeleteView):
+    model=Question
+    template_name= "polls/detail.html"
+
+class ResultView(generic.DeleteView):
+    model=Question
+    template_name= "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -48,6 +49,21 @@ def vote(request, question_id):
     return render(request, 'polls/vote.html', context )
 
 
+"""
+def detail(request, question_id):
+    '''try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Esta página de detalle no existe")'''
+    question = get_object_or_404(Question, pk=question_id)
+    #choices = question.choice_set.all()
+    return render(request, 'polls/detail.html', {'question':question} )
+    #return HttpResponse("Detail %s" % question_id)
+    
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question} )
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #output = ', '.join([q.question_text for q in latest_question_list])
@@ -62,4 +78,4 @@ def index(request):
     #template = loader.get_template('polls/index.html')
     #return HttpResponse(template.render(context, request))
     
-    return render(request, 'polls/index.html', context )
+    return render(request, 'polls/index.html', context )"""
