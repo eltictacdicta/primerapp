1d2e9322c699e77e18ebb668aa75f7eec60987e0
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-09-23 15:31:06 +0200

    Empezamos a configurar el admin

--- primerapp/polls/admin.py
+++ primerapp/polls/admin.py
@@ -3,8 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model= Choice
+    extra= 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields= ['pub_date', 'question_text']
+    #fields= ['pub_date', 'question_text']
+    list_display = ('question_text','pub_date')
+    list_filter= ['pub_date']
+    search_fields= ['question_text']
+    fieldsets = [
+        (None, { 'fields' : ['question_text']}),
+        ('Fecha', { 'fields' : ['pub_date']})
+
+    ]
+    inlines= [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
